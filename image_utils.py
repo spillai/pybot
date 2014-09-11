3b547d3696fd8622ab42000c7aafcfe6539f27a0
Sudeep Pillai <spillai@csail.mit.edu>
2014-09-11 16:57:02 -0400

    Added todos and ideas for rapid filter creation

--- image_utils.py
+++ image_utils.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 
 def im_resize(im, scale=0.5, interpolation=cv2.INTER_AREA): 
-    print 'resizing with scale', scale
     return cv2.resize(im, None, fx=scale, fy=scale, interpolation=interpolation)
 
 def gaussian_blur(im, size=3): 
