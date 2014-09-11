2f8c42cd6269e5324bb0f31dbe75c98ff98feb59
Sudeep Pillai <spillai@csail.mit.edu>
2014-09-11 16:42:31 -0400

    First pass at filter factory with sklearn pipeline compatibility

--- image_utils.py
+++ image_utils.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
 
+def im_resize(im, scale=0.5, interpolation=cv2.INTER_AREA): 
+    print 'resizing with scale', scale
+    return cv2.resize(im, None, fx=scale, fy=scale, interpolation=interpolation)
+
 def gaussian_blur(im, size=3): 
     return cv2.GaussianBlur(im, (size,size), 0)
 
