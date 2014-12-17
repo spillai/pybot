2e07d9718ccff22b46ac519eb26809844748965f
Sudeep Pillai <spillai@csail.mit.edu>
2014-12-17 14:37:05 -0500

    Added plot_confusion matrix to plot_utils

--- plot_utils.py
+++ plot_utils.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import matplotlib as mpl
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 
 def colormap(v, scale=255): 
     return plt.cm.hsv(v.ravel())[:,:3] * scale
@@ -10,6 +10,30 @@ def colormap(v, scale=255):
 
 def moving_average(X, win_size=3): 
     return np.convolve(X, np.repeat(1.0, win_size) / win_size, 'valid')
+
+def plot_confusion_matrix(cm, target_names, title='Confusion matrix', cmap=plt.cm.Greys, block=True):
+    # Colormaps: jet, Greys
+    cm_normalized = cm.astype(np.float32) / cm.sum(axis=1)[:, np.newaxis]
+    plt.imshow(cm_normalized, interpolation='nearest', cmap=cmap)
+
+    # Show confidences
+    for i, cas in enumerate(cm): 
+        for j, c in enumerate(cas): 
+            if c > 0: 
+                plt.text(j-0.1, i+0.2, c, fontsize=16, fontweight='bold', color='#b70000')
+
+    f = plt.figure(1)
+    f.clf()
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(target_names))
+    plt.xticks(tick_marks, target_names, rotation=45)
+    plt.yticks(tick_marks, target_names)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show(block=block)
+
 
 def plot_poses(poses, pose_type='absolute'): 
 
