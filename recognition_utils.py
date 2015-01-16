1962ff732a054049bd76a8a1be78bc8764298dd6
Sudeep Pillai <spillai@csail.mit.edu>
2015-01-16 11:49:25 -0500

    Moved back to linearsvc, sgd giving issues with classification performance (32 mAP - SGD) vs (52 mAP - SVC)

--- recognition_utils.py
+++ recognition_utils.py
@@ -11,12 +11,14 @@ from bot_utils.io_utils import memory_usage_psutil
 from bot_utils.db_utils import AttrDict, AttrDictDB, IterDB
 
 import sklearn.metrics as metrics
-from sklearn.linear_model import SGDClassifier
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
-from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import train_test_split
-from sklearn.kernel_approximation import AdditiveChi2Sampler
+from sklearn.kernel_approximation import AdditiveChi2Sampler, RBFSampler
+from sklearn.pipeline import Pipeline
+
+from sklearn.externals.joblib import Parallel, delayed
 
 class HomogenousKernelMap(AdditiveChi2Sampler): 
     def __init__(self, sample_steps=2, sample_interval=None): 
@@ -64,20 +66,22 @@ class ImageDescription(object):
            desc: [N x D]
         """
 
-        try: 
-            # Descriptor extraction
-            kpts = self.detector.detect(img, mask=mask)
-            kpts, desc = self.extractor.compute(img, kpts)
+        # try: 
 
-            # Extract color information (Lab)
-            pts = np.vstack([kp.pt for kp in kpts]).astype(np.int32)
-            # imgc = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-            # lab = median_blur(imgc, size=5) 
-            cdesc = img[pts[:,1], pts[:,0]]
-            return kpts, np.hstack([desc, cdesc])
+        # Descriptor extraction
+        kpts = self.detector.detect(img, mask=mask)
+        kpts, desc = self.extractor.compute(img, kpts)
+        # print 'KPTS, DESC: ', len(kpts), desc.shape
+            
+        # Extract color information (Lab)
+        pts = np.vstack([kp.pt for kp in kpts]).astype(np.int32)
+        # imgc = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+        # lab = median_blur(imgc, size=5) 
+        cdesc = img[pts[:,1], pts[:,0]]
+        return kpts, np.hstack([desc, cdesc])
 
-        except: 
-            return None, None
+        # except: 
+        #     return None, None
 
     def describe(self, img, mask=None): 
         """
@@ -89,6 +93,10 @@ class ImageDescription(object):
         """
         kpts, desc = self.detect_and_describe(img, mask=mask)
         return desc
+
+def bow_project(desc, vocab, index): 
+    self.bow = BoWVectorizer.from_dict(vocab, index=index)
+    return self.bow.project(desc)
 
 class ImageClassifier(object): 
     """
@@ -116,7 +124,6 @@ class ImageClassifier(object):
         self.dataset = dataset
         self.process_cb = process_cb
         self.params = AttrDict(params)
-        print 'Memory usage at __init__ start %5.2f MB' % (memory_usage_psutil())
 
         # Optionally setup training testing
         if dataset is not None: 
@@ -139,18 +146,21 @@ class ImageClassifier(object):
         self.image_descriptor = ImageDescription(**self.params.descriptor)
 
         # Setup kernel feature map
+        # kernel_tf = [('chi2_kernel', HomogenousKernelMap(2)), ('rbf_sampler', RBFSampler(gamma=0.2, n_components=40))]
+        # self.kernel_tf = Pipeline(kernel_tf) if self.params.use_kernel_approximation else None
         self.kernel_tf = HomogenousKernelMap(2) if self.params.use_kernel_approximation else None
 
         # Setup classifier
         print 'Building Classifier'
-        # self._clf = KNeighborsClassifier(n_neighbors=10)
+        # cv_params = {'loss':['hinge'], 'alpha':[0.01, 0.1, 1.0, 10.0], 'class_weight':['auto']}
+        # self._clf = SGDClassifier(loss='hinge', n_jobs=4)
+
+        # Linear SVM
+        cv_params = {'C':[0.01, 0.1, 0.5, 1.0, 10.0]}
         self._clf = LinearSVC()
-        self.clf = OneVsRestClassifier(self._clf, n_jobs=1)
-        # self.clf = ExtraTreesClassifier(n_estimators=10, 
-        #                                 max_depth=3, min_samples_split=1, random_state=0)
 
-        print 'Memory usage at __init__ completed %5.2f MB' % (memory_usage_psutil())
-
+        # Grid search cross-val
+        self.clf = GridSearchCV(self._clf, cv_params, cv=10, n_jobs=4, verbose=1)
 
     def setup_training_testing(self, max_test_size): 
         # Split up training and test
@@ -182,84 +192,132 @@ class ImageClassifier(object):
         print '===> Training '
         st = time.time()
 
-        print 'Memory usage at pre-train %5.2f MB' % (memory_usage_psutil())
-
         # Extract features
-        if not self.params.cache.features_path: 
-            raise RuntimeError('Setup features_path before running training')
-        
-        # train_desc = [self.image_descriptor.describe(**self.process_cb(x_t)) for x_t in self.X_train]
-        features_db = IterDB(filename=self.params.cache.features_path, mode='w', 
-                             fields=['train_desc', 'train_target'], )
-        for idx, (x_t,y_t) in enumerate(izip(self.X_train, self.y_train)): 
-            features_db.append('train_desc', self.image_descriptor.describe(**self.process_cb(x_t)))
-            features_db.append('train_target', y_t)
-            if idx % 20 == 0 and idx > 0: 
-                features_db.flush()
-        features_db.save()
+        if not self.params.cache.features_dir: 
+            raise RuntimeError('Setup features_dir before running training')
 
-        print 'Descriptor extraction took %5.3f s' % (time.time() - st)    
+        # Extract features, only if not already available
+        vocab_construction = AttrDict(num_per_image=1e3, num_images=1e3)
+        if not os.path.isdir(os.path.join(self.params.cache.features_dir, 'training.h5')): 
+            print '====> [COMPUTE] Feature Extraction '        
+            features_db = IterDB(filename=os.path.join(self.params.cache.features_dir, 'training.h5'), mode='w', 
+                                 fields=['train_desc', 'train_target', 'vocab_desc'])
+            for idx, (x_t,y_t) in enumerate(izip(self.X_train, self.y_train)): 
+                # Extract and add descriptors to db
+                im_desc = self.image_descriptor.describe(**self.process_cb(x_t))
+                features_db.append('train_desc', im_desc)
+                features_db.append('train_target', y_t)
 
-        print 'Memory usage at post-describe %5.2f MB' % (memory_usage_psutil())
+                # Randomly sample from descriptors for vocab construction
+                inds = np.random.permutation(int(min(len(im_desc), vocab_construction.num_per_image)))
+                features_db.append('vocab_desc', im_desc[inds])
+
+                if idx % 5 == 0 and idx > 0: 
+                    features_db.flush()
+            features_db.save()
+            print 'Descriptor extraction took %5.3f s' % (time.time() - st)    
+        else: 
+            print '====> [LOAD] Feature Extraction'        
+            features_db = IterDB(filename=os.path.join(self.params.cache.features_dir, 'training.h5'), mode='r')
+        print '-------------------------------'
 
         # Build BOW
-        inds = np.random.permutation(len(self.X_train))[:1e2]
-        train_desc = np.vstack([item for item in features_db.itervalues(key='train_desc', inds=inds)])
-        self.bow.build(train_desc); 
-        train_desc = None
-        print 'Codebook: %s' % ('GOOD' if np.isfinite(self.bow.codebook).all() else 'BAD')
-
-        print 'Memory usage at post-bow-build %5.2f MB' % (memory_usage_psutil())
+        if not os.path.exists(self.params.cache.vocab_path):# or self.params.cache.overwrite: 
+            print '====> [COMPUTE] Vocabulary Construction'
+            inds = np.random.permutation(len(self.X_train))[:vocab_construction.num_images]
+            vocab_desc = np.vstack([item for item in features_db.itervalues(key='vocab_desc', inds=inds, verbose=True)])
+            print 'Codebook data: %i, %i' % (len(inds), len(vocab_desc))
+            print '====> MEMORY: Codebook construction: %4.3f MB' % (vocab_desc.nbytes / 1024 / 1024.0) 
+            self.bow.build(vocab_desc)
+            vocab_desc = None
+            vocab_db = AttrDict(params=self.params, bow=self.bow.to_dict())
+            vocab_db.save(self.params.cache.vocab_path)
+            print 'Codebook: %s' % ('GOOD' if np.isfinite(self.bow.codebook).all() else 'BAD')
+        else: 
+            print '====> [LOAD] Vocabulary Construction'
+            vocab_db = AttrDict.load(self.params.cache.vocab_path)
+            self.bow = BoWVectorizer.from_dict(vocab_db.bow)
+        print '-------------------------------'
 
         # Histogram of trained features
-        train_target = np.array(self.y_train, dtype=np.int32)
-        train_histogram = np.vstack([self.bow.project(desc) for desc in features_db.itervalues(key='train_desc')])
-        # train_histogram = np.vstack([self.bow.project(desc) for desc in train_desc])
-        print 'Memory usage at post-project %5.2f MB' % (memory_usage_psutil())
+        if not os.path.exists(self.params.cache.hists_path): 
+            print '====> [COMPUTE] BoVW / VLAD projection '
+            train_target = np.array(self.y_train, dtype=np.int32)
+        
+            vocab_index = BoWVectorizer.compute_index(vocab_db.bow.codebook)
+            res = Parallel(n_jobs=8, verbose=5)(delayed(bow_project)(desc, vocab_db.bow, vocab_index) for desc in features_db.itervalues(key='train_desc', verbose=True))
+            # train_histogram = np.vstack([self.bow.project(desc) 
+            #                              for desc in features_db.itervalues(key='train_desc', verbose=True)])
+            hists_db = AttrDict(train_target=train_target, train_histogram=train_histogram)
+            hists_db.save(self.params.cache.hists_path)
+            print '====> MEMORY: Histogram: %s %4.3f MB' % (train_histogram.shape, 
+                                                            train_histogram.nbytes / 1024 / 1024.0) 
+        else: 
+            print '====> [LOAD] BoVW / VLAD projection '
+            hists_db = AttrDict.load(self.params.cache.hists_path)
+            train_target, train_histogram = hists_db.train_target, hists_db.train_histogram
+        print '-------------------------------'
 
         # Homogeneous Kernel map
         if self.params.use_kernel_approximation: 
+            print '====> Kernel Approximation '
             train_histogram = self.kernel_tf.fit_transform(train_histogram)
-        
+            print '-------------------------------'        
+
         # Train/Predict one-vs-all classifier
+        print '====> Train SGD classifier '
         st_sgd = time.time()
         self.clf.fit(train_histogram, train_target)
         pred_target = self.clf.predict(train_histogram)
         print 'Training SGD Classifier took %5.3f s' % (time.time() - st_sgd)
-        
-        print 'Memory usage at post-fit %5.2f MB' % (memory_usage_psutil())
+        print '-------------------------------'        
+
 
         print ' Accuracy score (Training): %4.3f' % (metrics.accuracy_score(train_target, pred_target))
         print ' Report (Training):\n %s' % (metrics.classification_report(train_target, pred_target, 
                                                                           target_names=self.dataset.target_names))
 
-        print 'Memory usage at post-predict %5.2f MB' % (memory_usage_psutil())
         print 'Training took %5.3f s' % (time.time() - st)
 
-        print 'Saving classifier'
+        print '====> Saving classifier '
         self.save(self.params.cache.detector_path)
+        print '-------------------------------'
         return
 
-    def classify(self, classify_trained=False): 
+    def classify(self): 
         print '===> Classification '
         st = time.time()
 
         # Extract features
-        test_data = self.X_all if classify_trained else self.X_test
-        test_desc = [ self.image_descriptor.describe(**self.process_cb(x_t)) for x_t in test_data ]
+        print '====> Feature Extraction '        
+        features_db = IterDB(filename=os.path.join(self.params.cache.features_dir, 'testing.h5'), mode='w', 
+                             fields=['test_desc', 'test_target'])
+        for idx, (x_t,y_t) in enumerate(izip(self.X_test, self.y_test)): 
+            features_db.append('test_desc', self.image_descriptor.describe(**self.process_cb(x_t)))
+            features_db.append('test_target', y_t)
+            if idx % 5 == 0 and idx > 0: 
+                features_db.flush()
+        features_db.save()
+        print '-------------------------------'
 
         print 'Descriptor extraction took %5.3f s' % (time.time() - st)    
-    
+
+        print '====> BoVW / VLAD projection '
         # Histogram of trained features
-        test_target = self.y_all if classify_trained else self.y_test
-        test_histogram = np.vstack([self.bow.project(desc) for desc in test_desc])
+        test_target = self.y_test
+        test_histogram = np.vstack([self.bow.project(desc) for desc in features_db.itervalues(key='test_desc')])
+        # test_histogram = np.vstack([self.bow.project(desc) for desc in test_desc])
+        print '====> MEMORY: Histogram: %s %4.3f MB' % (test_histogram.shape, test_histogram.nbytes / 1024 / 1024.0) 
+        print '-------------------------------'
 
         if self.params.use_kernel_approximation: 
             # Apply homogeneous transform
-            test_histogram = self.kernel_tf.fit_transform(test_histogram)
+            test_histogram = self.kernel_tf.transform(test_histogram)
 
+        print '====> Predict using SGD classifer '
         pred_target = self.clf.predict(test_histogram)
         pred_score = self.clf.decision_function(test_histogram)
+        print '-------------------------------'
 
         # print ' Confusion matrix (Test): %s' % (metrics.confusion_matrix(test_target, pred_target))
         print '=========================================================> '
