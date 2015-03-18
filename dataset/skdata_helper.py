171c3b8592b4a27ef15ea3f7ccf587066803f834
Sudeep Pillai <spillai@csail.mit.edu>
2015-03-18 17:08:45 -0400

    Working caltech 101 training / testing with skdata

--- dataset/skdata_helper.py
+++ dataset/skdata_helper.py
@@ -11,30 +11,30 @@ class skdataHelper(object):
       target_names: [car, bike, ... ]
     """
     def __init__(self, dataset, targets=None): 
-        fn, class_names = zip(*map(lambda x: (x['filename'], x['name']), dataset.meta))
-        self._class_names = list(set(class_names))
+        self._dataset = dataset
+        _ = self._dataset.meta
+
+        self._class_names = np.array(self._dataset.names)
         self._class_ids = np.arange(len(self._class_names), dtype=np.int)
 
         self.target_hash = dict(zip(self._class_names, self._class_ids))
         self.target_unhash = dict(zip(self._class_ids, self._class_names))
-        class_ids = map(lambda cname: self.target_hash[cname], class_names)
 
         # Options: 
         # 1. Select full set: targets = None, 
         # 2. Specific set:    targets = ['a', 'b']
         # 3. Target count:    targets = 10 (randomly pick 10 classes)
 
-        # Pick full/specified dataset            
+        # Pick full/specified dataset
         if targets is None: 
-            target_ids = self._class_ids
-            self.data, self.target = fn, map(lambda cname: self.target_hash[cname], class_names)
+            self._whitelist = None
 
         # Or specific count/set
         else: 
             # If integer valued, retrieve targets
             if isinstance(targets, int) and targets < len(self._class_names): 
                 inds = np.random.randint(len(self._class_names), size=targets)
-                target_ids = self._class_ids[inds]
+                self._whitelist = set(self._class_names[inds])
 
             # If targets are list of strings
             elif isinstance(targets, list) and len(targets) < len(self._class_names): 
@@ -42,20 +42,32 @@ class skdataHelper(object):
                 for t in targets: 
                     if t not in self._class_names: 
                         raise ValueError('Target %s not in class_names' % t)
-                target_ids = map(lambda cname: self.target_hash[cname], targets)
+                self._whitelist = set(targets)
             else: 
                 raise ValueError('targets are not list of strings or integer')
 
-            # Filter data to specific targets_set
-            target_ids_set = set(target_ids)
-            self.data, self.target = zip(*filter(lambda x: x[1] in target_ids_set, zip(fn, class_ids)))
+        self.target_names = np.array(list(self._whitelist)) if self._whitelist is not None else self._class_names 
+        self.target_ids = map(lambda cname: self.target_hash[cname], self.target_names)
 
+    def _split(self, split=None): 
+        if split:
+            inds = self._dataset.splits[split]
+        else:
+            inds = xrange(len(self._dataset.meta))
+            
+        data = [ (self._dataset.meta[ind]['filename'], self.target_hash[self._dataset.meta[ind]['name']])
+                 for ind in inds 
+                 if (self._whitelist is None) or (self._dataset.meta[ind]['name'] in self._whitelist) ]
 
-        self.data = np.array(self.data)
-        self.target = np.array(self.target)
+        fn, target = zip(*data)
+        return np.array(fn), np.array(target, dtype=np.int32)
 
-        self.target_ids = sorted(np.unique(self.target))
-        self.target_names = map(lambda tid: self.target_unhash[tid], self.target_ids)
+    def get_train_test_split(self): 
+        for split_idx in range(0, self._dataset.num_splits): 
+            X_train, y_train = self._split(split='train_%i' % split_idx)
+            X_test, y_test = self._split(split='test_%i' % split_idx)
+            return X_train, y_train, X_test, y_test
+            
 
 
 
