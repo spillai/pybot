3ec84fde4cb369c52644979ddbef6fd8b679a61b
Sudeep Pillai <spillai@csail.mit.edu>
2015-04-08 17:49:07 -0400

    Cleanups to object reconstruction based slam interface

--- lcm/draw_utils.py
+++ lcm/draw_utils.py
@@ -661,7 +661,7 @@ def draw_camera(pose, depth=0.1, fov=np.deg2rad(60)):
     return (faces, np.hstack([pts[:-1], pts[1:]]).reshape((-1,3)))
 
 def publish_cameras(pub_channel, poses, c='y', texts=[], frame_id='KINECT', draw_faces=True, draw_edges=True):
-    cam_feats = [draw_camera(pose, depth=0.12) for pose in poses]
+    cam_feats = [draw_camera(pose, depth=0.05) for pose in poses]
     cam_faces = map(lambda x: x[0], cam_feats)
     cam_edges = map(lambda x: x[1], cam_feats)
 
