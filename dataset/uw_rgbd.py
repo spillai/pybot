b688acb1de5019b21c773ed9cffa06b3db87f79d
Sudeep Pillai <spillai@csail.mit.edu>
2015-04-14 11:36:05 -0400

    Fully functional batch hard-neg mining (in-memory) for training

--- dataset/uw_rgbd.py
+++ dataset/uw_rgbd.py
@@ -49,9 +49,9 @@ class UWRGBDDataset(object):
     target_unhash = dict(zip(class_ids, class_names))
 
     # train_names = ["cereal_box", "cap", "background"]
-    train_names = ["bowl", "cap", "cereal_box", "background"]
+    # train_names = ["bowl", "cap", "cereal_box", "background"]
     # train_names = ["cap", "cereal_box", "coffee_mug", "soda_can", "background"]
-    # train_names = ["bowl", "cap", "cereal_box", "coffee_mug", "soda_can", "background"]
+    train_names = ["bowl", "cap", "cereal_box", "coffee_mug", "soda_can", "background"]
     # train_names = ["bowl", "cap", "cereal_box", "coffee_mug", "soda_can"]
     # train_names = ["bowl", "cap", "cereal_box", "coffee_mug", "flashlight", 
     #                "keyboard", "kleenex", "scissors",  "soda_can", 
