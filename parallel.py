7007c25d0f07f02d942881f9114b33a648afa60a
Sudeep Pillai <spillai@csail.mit.edu>
2016-01-09 18:45:25 -0500

    Parallel for recipe

---
+++ parallel.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from bot_utils.itertools_recipes import chunks
+from sklearn.externals.joblib import Parallel, delayed
+
+def parallel_for(iterable, func, batch_size, *func_args, **func_kwargs): 
+    for chunk in chunks(iterable, batch_size): 
+        res = Parallel(n_jobs=6, verbose=0) (
+            delayed(func)(item, *func_args, **func_kwargs)
+            for item in chunk )
+        yield res
+
+if __name__ == "__main__": 
+    
+    def get_iterable(): 
+        idx = 0
+        while idx < 50000: 
+            idx += 1
+            yield idx
+
+    # for chunk in chunks(get_iterable(), 16): 
+    #     print chunk 
+    res = parallel_for(get_iterable(), np.sin, 16)
+    for item in res: 
+        print item
+    
