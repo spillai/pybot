2a67f541155ec436df2727c443d268a77bc38ccd
Sudeep Pillai <spillai@csail.mit.edu>
2016-02-19 12:06:47 -0500

    Faststereo wrapper with separate FastStereoViz class

--- stereo/fast_stereo.py
+++ stereo/fast_stereo.py
@@ -6,17 +6,20 @@ from pybot_vision import scaled_color_disp
 
 class FastStereo(object): 
     def __init__(self, calib, threshold=10, scale=1.0, iterations=1): 
-        print calib
-
         # Stereo Methods: CROSS_RATIO_DISPARITY, TESSELLATED_DISPARITY, PLANAR_INTERP_DISPARITY
         self.stereo = _FastStereo(threshold=threshold, 
-                                  stereo_method=_FastStereo.TESSELLATED_DISPARITY, lr_consistency_check=True)
+                                  stereo_method=_FastStereo.TESSELLATED_DISPARITY, iterations=iterations, lr_consistency_check=True)
         self.stereo.set_calibration(calib.left.K, calib.right.K, 
                                     calib.left.D, calib.right.D, calib.left.R, calib.right.R, 
                                     calib.left.P, calib.right.P, calib.Q, calib.right.t)
-        self.stereo.iterations = iterations
 
-        self.calib = calib
+    def process(self, left_im, right_im): 
+        return self.stereo.process(to_gray(left_im), to_gray(right_im))
+
+class FastStereoViz(FastStereo): 
+    def __init__(self, calib, threshold=10, scale=1.0, iterations=1): 
+        FastStereo.__init__(self, calib, threshold=threshold, scale=scale, iterations=iterations)
+        print calib
         self.scale = scale
 
         # Trackbar
@@ -25,9 +28,7 @@ class FastStereo(object):
         trackbar_create('iterations', 'disparity', iterations, 10, scale=1)
 
     def process(self, left_im, right_im): 
-        
-        # Stereo process
-        disp = self.stereo.process(to_gray(left_im), to_gray(right_im))
+        disp = FastStereo.process(self, to_gray(left_im), to_gray(right_im))
 
         # Display colored depth
         vis = scaled_color_disp(disp / self.scale)
@@ -42,4 +43,5 @@ class FastStereo(object):
 
     def evaluate(self, gt_disp): 
         imshow_cv("gt", scaled_color_disp(gt_disp))               
+    
 
