3640e5d5f1aaac1ee2fbf30e081be996e53ac24f
Sudeep Pillai <spillai@csail.mit.edu>
2016-03-03 20:47:56 -0500

    Added support for piecemeal timers

--- timer.py
+++ timer.py
@@ -2,26 +2,40 @@ import time
 
 class SimpleTimer: 
     def __init__(self, name='', iterations=10): 
-        self._counter = 0
-        self._last = time.time()
-        self._name = name
+        self.counter_ = 0
+        self.last_ = time.time()
+        self.name_ = name
         # self.iterations = iterations
         self.period_ = 0
 
     def poll(self): 
-        self._counter += 1
+        self.counter_ += 1
         now = time.time()
-        if now - self._last > 1: 
-            self.period_ = (now - self._last) * 1e3 / self._counter
-            print('%s: %4.3f ms (avg over %i iterations)' % (self._name, self.period_, self._counter))
-            self._last = now
-            self._counter = 0
+        dt = (now - self.last_) * 1e3
+
+        if dt > 1000: 
+            self.period_ = dt / self.counter_
+            print('%s: %4.3f ms (avg over %i iterations)' % (self.name, self.period_, self.counter_))
+            self.last_ = now
+            self.counter_ = 0
+
+    def poll_piecemeal(self): 
+        self.counter_ += 1
+        now = time.time()
+        dt = (now - self.last_) * 1e3
+        self.period_ += dt
+
+        if self.period_ > 100:
+            print('%s: %4.3f ms (avg over %i iterations)' % (self.name_, self.period_ / self.counter_, self.counter_))
+            self.last_ = now
+            self.counter_ = 0
+            self.period_ = 0
             
     def start(self): 
-        self._last = time.time()
+        self.last_ = time.time()
 
     def stop(self): 
-        self.poll()
+        self.poll_piecemeal()
 
     @property
     def fps(self): 
