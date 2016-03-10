2de1cebb08a927ef092615753705e12b71d995bd
Sudeep Pillai <spillai@csail.mit.edu>
2016-03-10 12:46:19 -0500

    Added support for colored printing

--- misc.py
+++ misc.py
@@ -1,6 +1,15 @@
 from collections import deque
 import progressbar as pb
 
+def print_red(print_t): print("\033[91m {}\033[00m" .format(prt))
+def print_green(prt): print("\033[92m {}\033[00m" .format(prt))
+def print_yellow(prt): print("\033[93m {}\033[00m" .format(prt))
+def print_lightpurple(prt): print("\033[94m {}\033[00m" .format(prt))
+def print_purple(prt): print("\033[95m {}\033[00m" .format(prt))
+def print_cyan(prt): print("\033[96m {}\033[00m" .format(prt))
+def print_lightgray(prt): print("\033[97m {}\033[00m" .format(prt))
+def print_black(prt): print("\033[98m {}\033[00m" .format(prt))
+
 def setup_pbar(maxval): 
     widgets = ['Progress: ', pb.Percentage(), ' ', pb.Bar(), ' ', pb.ETA()]
     pbar = pb.ProgressBar(widgets=widgets, maxval=maxval)
