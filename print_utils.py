3fc9974ab761a4f5ff5450c3f74c2ceea0eff7a5
Sudeep Pillai <spillai@csail.mit.edu>
2016-03-13 21:14:25 -0400

    updated tcp utils with tcpcontrol

---
+++ print_utils.py
@@ -0,0 +1,9 @@
+
+def print_red(print_t): print("\033[91m {}\033[00m" .format(prt))
+def print_green(prt): print("\033[92m {}\033[00m" .format(prt))
+def print_yellow(prt): print("\033[93m {}\033[00m" .format(prt))
+def print_lightpurple(prt): print("\033[94m {}\033[00m" .format(prt))
+def print_purple(prt): print("\033[95m {}\033[00m" .format(prt))
+def print_cyan(prt): print("\033[96m {}\033[00m" .format(prt))
+def print_lightgray(prt): print("\033[97m {}\033[00m" .format(prt))
+def print_black(prt): print("\033[98m {}\033[00m" .format(prt))
