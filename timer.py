2ef926c2b1d747ebf0ac398561274ee135faa560
Sudeep Pillai <spillai@csail.mit.edu>
2016-03-21 20:09:20 -0400

    Fixes to timer.py and appropriate rewrites for apps

--- timer.py
+++ timer.py
@@ -1,22 +1,26 @@
 import time
 
 class SimpleTimer: 
-    def __init__(self, name='', iterations=10): 
+    def __init__(self, name='', hz=1.0): 
+        self.name_ = name
+        
         self.counter_ = 0
         self.last_ = time.time()
-        self.name_ = name
-        # self.iterations = iterations
         self.period_ = 0
+        
+        self.last_print_ = time.time()        
+        self.last_fps_ = 0
 
     def poll(self): 
         self.counter_ += 1
         now = time.time()
         dt = (now - self.last_)
 
-        if dt > 0.1: 
-            self.period_ = dt / self.counter_
-            print('%s: %4.3f ms (avg over %i iterations)' % (self.name_, self.period_ * 1e3, self.counter_))
+        if (now-self.last_print_) > 1: 
+            T = dt / self.counter_
+            print('%s: %4.3f ms (avg over %i iterations)' % (self.name_, T * 1e3, self.counter_))
             self.last_ = now
+            self.last_print_ = now
             self.counter_ = 0
 
     def poll_piecemeal(self): 
@@ -25,9 +29,13 @@ class SimpleTimer:
         dt = (now - self.last_)
         self.period_ += dt
 
-        if self.period_ > 0.1:
-            print('%s: %4.3f ms (avg over %i iterations)' % (self.name_, self.period_ * 1e3 / self.counter_, self.counter_))
+        if (now-self.last_print_) > 1:
+            T = self.period_ / self.counter_
+            fps = 1.0 / T
+            print('%s: %4.3f ms (avg over %i iterations)' % (self.name_, T * 1e3, self.counter_))
             self.last_ = now
+            self.last_print_ = now
+            self.last_fps_ = fps
             self.counter_ = 0
             self.period_ = 0
             
@@ -39,7 +47,5 @@ class SimpleTimer:
 
     @property
     def fps(self): 
-        try: 
-            return 1.0 / self.period_ 
-        except: 
-            return 0.0
+        return self.last_fps_
+        
