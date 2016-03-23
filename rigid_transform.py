df9610fb162cbf4a950f3a0278dee71b080a6100
Sudeep Pillai <spillai@csail.mit.edu>
2016-03-22 20:25:47 -0400

    Added comments for cross product matrix / skew symm matrix

--- rigid_transform.py
+++ rigid_transform.py
@@ -17,6 +17,13 @@ def skew(v, return_dv=False):
   """ 
   Returns the skew-symmetric matrix of a vector
   Ref: https://github.com/dreamdragon/Solve3Plus1/blob/master/skew3.m
+
+  Also known as the cross-product matrix [v]_x such that 
+  the cross product of (v x w) is equivalent to the 
+  matrix multiplication of the cross product matrix of 
+  v ([v]_x) and w
+
+  In other words: v x w = [v]_x * w
   """
   sk = np.float32([[0, -v[2], v[1]],
                    [v[2], 0, -v[0]],
