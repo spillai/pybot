c08add327d8740b103e177ceb946b696654356c3
Sudeep Pillai <spillai@csail.mit.edu>
2016-05-05 16:05:43 -0400

    mser/test_rcnn updates

--- mser_utils.py
+++ mser_utils.py
@@ -37,15 +37,15 @@ class MSER:
     def detect(self, im, colorspace='hsv'): 
         if colorspace == 'hsv': 
             cim = cv2.cvtColor(im, cv2.COLOR_BGR2HLS)
-        else: 
+        elif colorspace is None: 
             cim = im
         return self.mser.detect(cim)
 
-    def hulls(self, im):
-        regions = self.detect(im)
+    def hulls(self, im, colorspace='hsv'):
+        regions = self.detect(im, colorspace=colorspace)
         return [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
 
-    def ellipses(self, im): 
-        hulls = self.hulls(im)
+    def ellipses(self, im, colorspace='hsv'): 
+        hulls = self.hulls(im, colorspace=colorspace)
         return [cv2.fitEllipse(contours_from_endpoints(hull.reshape(-1,2),10))
                 for hull in hulls]
