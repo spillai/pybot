3e8e493c1ac22860d58fc4149153030de698d893
Sudeep Pillai <spillai@csail.mit.edu>
2016-07-06 23:34:36 -0400

    cleaned up several drawing code

--- lcm/draw_utils.py
+++ lcm/draw_utils.py
@@ -144,20 +144,6 @@ def publish_botviewer_image_t(im, jpeg=False, flip_rb=True):
         raise TypeError('publish_botviewer_image_t type is not np.ndarray')
     publish_image_t('CAMERA_IMAGE', im, jpeg=jpeg, flip_rb=flip_rb)
 
-def draw_tag(pose=None, size=0.1):
-    sz = size / 2.0
-    corners = np.float32([[sz, sz, 0], [-sz, sz, 0], 
-                          [-sz, -sz, 0], [sz, -sz, 0], 
-                          [sz, sz, 0], [-sz, -sz, 0], 
-                          [-sz, sz, 0], [sz, -sz, 0]])
-    return pose * corners if pose is not None else corners
-
-def draw_tag_edges(pose=None, size=0.1):
-    return corners_to_edges(draw_tag(pose=pose, size=size))
-    
-def draw_tags_edges(poses, size=0.1): 
-    return np.vstack([draw_tag_edges(p) for p in poses])
-
 def corners_to_edges(corners):
     """ Edges are represented in N x 6 form """
     return np.hstack([corners, np.roll(corners, 1, axis=0)])
@@ -165,93 +151,6 @@ def corners_to_edges(corners):
 def polygons_to_edges(polygons):
     """ Edges are represented in N x 6 form """
     return np.vstack([ corners_to_edges(corners) for corners in polygons])
-    
-# @run_async
-# def publish_cloud(pub_ns, _arr, _carr, stamp=None, flip_rb=False, frame_id='map', seq=None): 
-#     """
-#     Publish point cloud on:
-#     pub_ns: Namespace on which the cloud will be published
-#     arr: numpy array (N x 3) for point cloud data
-#     c: Option 1: 'c', 'b', 'r' etc colors accepted by matplotlibs color
-#        Option 2: float ranging from 0 to 1 via matplotlib's jet colormap
-#        Option 3: numpy array (N x 3) with r,g,b vals ranging from 0 to 1
-#     s: supported only by matplotlib plotting
-#     alpha: supported only by matplotlib plotting
-#     """
-#     arr, carr = deepcopy(_arr), deepcopy(_carr)
-#     N, D = arr.shape
-#     carr = get_color_arr(carr, N, flip_rb=flip_rb)
-
-#     # pc = xyzrgb_array_to_pointcloud2(arr, carr, stamp=stamp, frame_id=frame_id, seq=seq)
-#     # _publish_pc(pub_ns, pc)
-
-
-# Globals ==============================================================
-
-
-# def get_frame_id(ch): 
-#     global g_frames_id
-#     if ch in g_frames_id: return g_frames_id[ch]
-#     g_frames_id[ch] = len(g_frames_id) + 12345
-#     return g_frames_id[ch]
-
-# def get_frame(frame): 
-#     global g_frames_pub
-#     frame_id = publish_sensor_frame(frame)
-#     return g_frames_pub[frame_id]
-
-# def reshape_arr(arr):
-#     # if 3 dimensional (i.e. organized pt cloud), reshape to Nx3
-#     if arr.ndim == 3:
-#         arr = np.hstack((np.reshape(arr[:,:,0], (-1, 1)), 
-#                          np.reshape(arr[:,:,1], (-1,1)), 
-#                          np.reshape(arr[:,:,2], (-1,1))))
-#     return arr    
-
-
-# def convert_image_to_carr(img):
-#     r,g,b = np.split(img.astype(float)*1.0/255.0, 3, axis=2)
-#     carr = np.hstack((np.reshape(r, (-1,1)), np.reshape(g ,(-1,1)), np.reshape(b, (-1,1))))
-#     return carr
-
-# # ===== Color utils ====
-# def get_color_arr(c, n, color_func=plt.cm.gist_rainbow, 
-#                   color_by='value', palette_size=20, flip_rb=False):
-#     """ 
-#     Convert string c to carr array (N x 3) format
-#     """
-#     carr = None
-
-#     if color_by == 'value': 
-#         if isinstance(c, str): # single color
-#             carr = np.tile(np.array(colorConverter.to_rgb(c)), [n,1])
-#         elif  isinstance(c, float):
-#             carr = np.tile(np.array(color_func(c)), [n,1])
-#         else:
-#             carr = reshape_arr(c.astype(float) * 1.0)
-
-#     elif color_by == 'label': 
-#         if c < 0: 
-#             carr = np.tile(np.array([0,0,0,0]), [n,1])
-#         else: 
-#             carr = np.tile(np.array(color_func( (c % palette_size) * 1. / palette_size)), [n,1])
-#     else: 
-#         raise Exception("unknown color_by argument")
-
-#     if flip_rb: 
-#         r, b = carr[:,0], carr[:,2]
-#         carr[:,0], carr[:,2] = b.copy(), r.copy()
-
-#     return carr        
-    
-
-# # ===== Point cloud drawing (matplotlib) ====
-# def draw_point_cloud(ax, arr, c='r', size=1):
-#     """ 
-#     Draw (N x 3) array in matplotlib axes ax
-#     """
-#     arr = reshape_arr(arr)
-#     ax.plot(arr[:,0], arr[:,1], arr[:,2],'.',markersize=size, c=c)
 
 def arr_msg(arr, carr, frame_uid, element_id): 
     # point3d collection msg
@@ -444,281 +343,12 @@ def publish_covar_list(pub_channel, poses, covars=[], frame_id='camera', reset=T
 
     g_viz_pub.lc.publish("COV_COLLECTION", covar_list_msg.encode())
 
-
 def publish_line_segments(pub_channel, _arr1, _arr2, c='r', flip_rb=False, frame_id='camera', element_id=0, reset=True):
     publish_point_type(pub_channel, np.hstack([_arr1, _arr2]), c=c, point_type='LINES', 
                        flip_rb=flip_rb, frame_id=frame_id, element_id=element_id, reset=reset)
-
-
-# # @run_async
-# def publish_point_type(pub_channel, arr, c='r', point_type='POINT', 
-#                        flip_rb=False, frame_id='camera', element_id=0, reset=True):
-#     _publish_point_type(pub_channel, arr, c=c, point_type=point_type, 
-#                         flip_rb=flip_rb, frame_id=frame_id, element_id=element_id, reset=reset)
-
-# # @run_async
-# def publish_pose_list(pub_channel, poses, texts=[], covars=[], frame_id='camera', reset=True, object_type='AXIS3D'):
-#     _publish_pose_list(pub_channel, deepcopy(poses), texts=texts, covars=covars, 
-#                        frame_id=frame_id, reset=reset, object_type=object_type)
-
-# # ===== Tangents drawing ====
-# def _publish_line_segments(pub_channel, _arr1, _arr2, c='r', flip_rb=False, frame_id='camera', element_id=0):
-#     """ 
-#     Publish point cloud tangents:
-#     note: draw line from p1 to p2
-#     pub_channel: Channel on which the cloud will be published
-#     arr1: numpy array (N x 3) for point cloud data (p1)
-#     arr2: numpy array (N x 3) for point cloud data (p2) aligned 
-#     c: Option 1: 'c', 'b', 'r' etc colors accepted by matplotlibs color
-#        Option 2: float ranging from 0 to 1 via matplotlib's jet colormap
-#        Option 3: numpy array (N x 3) with r,g,b vals ranging from 0 to 1
-#     s: size of tangent vector (assuming normalized tangent vector)
-#     alpha: supported only by matplotlib plotting
-#     """
-#     global g_viz_pub
-#     arr1, carr = copy_pointcloud_data(_arr1, c, flip_rb=flip_rb)
-#     arr2 = _arr2.reshape(-1,3)
-#     if not arr1.shape == arr2.shape: raise AssertionError    
-
-#     frame_pose = g_viz_pub.get_sensor_pose(frame_id)
-
-#     # point3d list collection msg
-#     pc_list_msg = vs.point3d_list_collection_t()
-#     pc_list_msg.id = g_viz_pub.channel_uid(pub_channel)
-#     pc_list_msg.name = pub_channel
-#     pc_list_msg.type = vs.point3d_list_collection_t.LINES
-#     pc_list_msg.reset = True
-#     pc_list_msg.point_lists = []
-
-#     arr1s, arr2s, carrs = [arr1], [arr2], [carr]
-#     # # Handle 3D data: [ndarray or list of ndarrays]
-#     # arr1s, arr2s, carrs = [], [], []
-#     # if isinstance(arr1, np.ndarray) and isinstance(arr2, np.ndarray): 
-#     #     # ensure arr is in (N x 3) format
-#     #     # carr in (N x 3) format
-#     #     arr1, arr2 = reshape_arr(arr1), reshape_arr(arr2)
-#     #     arr1s.append(arr1)
-#     #     arr2s.append(arr2)
-#     # elif isinstance(arr1, list) and isinstance(arr2, list): 
-#     #     arr1s, arr2s = arr1, arr2
-#     # else: 
-#     #     raise TypeError("publish_line_segments: Unknown pts3d array type")
-
-#     # # Handle color: [string or ndarray]
-#     # if isinstance(c,str):
-#     #     carrs = [c] * len(arr1s)
-#     # elif isinstance(c, np.ndarray): 
-#     #     carrs.append(c)
-#     # else: 
-#     #     raise TypeError("publish_line_segments: Unknown color array type")
-#     #     carrs = c
-
-#     tpoints = 0
-#     for arr1,arr2,c in zip(arr1s,arr2s,carrs): 
-#         # fill out points
-#         npoints = len(arr1)
-#         tpoints += npoints
     
-#         # Get the colors
-#         carr = get_color_arr(c, npoints)
-#         ch, cw = carr.shape
-#         carr = np.hstack([carr, carr]).reshape((-1,cw))
-
-#         # Interleaved arr1, and arr2
-#         arr = np.hstack([arr1, arr2]).reshape((-1,3))
-        
-#         # Create the point cloud msg
-#         pc_msg = arr_msg(arr, carr=carr, frame_uid=g_viz_pub.channel_uid(frame_id), element_id=element_id)
-
-#         pc_list_msg.point_lists.append(pc_msg)
-
-#     # pc_msg.normals = [vs.point3d_t() for j in range(0,npoints)]   
-#     # pc_msg.nnormals = len(pc_msg.normals)
-#     # for j in range(0,npoints):
-#     #     pc_msg.normals[j].x, pc_msg.normals[j].y, pc_msg.normals[j].z = tarr[j,0], tarr[j,1], tarr[j,2]
-
-#     # add to point cloud list    
-#     pc_list_msg.nlists = len(pc_list_msg.point_lists)
-#     g_viz_pub.lc.publish("POINTS_COLLECTION", pc_list_msg.encode())
-#     print('Published %i lines' % (tpoints))
-
-# # @run_async
-# def publish_line_segments(pub_channel, arr1, arr2, c='r', flip_rb=False, frame_id='camera'):
-#     _publish_line_segments(pub_channel, deepcopy(arr1), deepcopy(arr2), c=deepcopy(c), flip_rb=flip_rb, frame_id=frame_id)
-
-
-# # # ===== Pose drawing (viz) ====
-# # def publish_pose_list(pub_channel, arr, sensor_tf=True, size=1, alpha=1, verbose=False):
-# #     """
-# #     Publish Pose List on:
-# #     pub_channel: Channel on which the cloud will be published
-# #     arr: numpy array (N x 7) for pose list data ([x y z qw qx qy qz])
-# #     c: Option 1: 'c', 'b', 'r' etc colors accepted by matplotlibs color
-# #        Option 2: float ranging from 0 to 1 via matplotlib's jet colormap
-# #        Option 3: numpy array (N x 3) with r,g,b vals ranging from 0 to 1
-# #     s: supported only by matplotlib plotting
-# #     alpha: supported only by matplotlib plotting
-# #     """
-# #     global frame_pose  
-# #     # pose list collection msg
-# #     pose_list_msg = vs.obj_collection_t()
-# #     pose_list_msg.id = hash(pub_channel) >> 32
-# #     pose_list_msg.name = pub_channel
-# #     pose_list_msg.type = vs.obj_collection_t.AXIS3D
-# #     pose_list_msg.reset = True
-
-# #     # # pose msg
-# #     # pose_msg = vs.obj_t()
-# #     # pose_msg.id = int(time.time() * 1e6)
-# #     # pose_msg.collection = hash('KINECT_FRAME') >> 32  # comes from the sensor_frames_msg published earlier
-# #     # pose_msg.element_id = 1
-
-# #     # ensure arr is in (N x 7) format
-# #     # carr in (N x 3) format
-# #     nposes = arr.shape[0] 
-
-# #     # fill out points
-# #     pose_list_msg.objs = [vs.obj_t() for j in range(0,nposes)]
-# #     pose_list_msg.nobjs = nposes             
-# #     inds = np.arange(0,nposes)
-
-# #     sensor_pose = tf.quaternion_from_euler(frame_pose.roll, frame_pose.pitch, frame_pose.yaw)
-# #     sensorT = tf.quaternion_matrix(sensor_pose)
-# #     sensorT[:3,3] = np.array([frame_pose.x, frame_pose.y, frame_pose.z])
-# #     # print sensorT
-
-# #     for j in range(nposes):
-# #         obsT = tf.quaternion_matrix(arr[j,-4:])
-# #         obsT[:3,3] = arr[j,:3]
-# #         if sensor_tf: 
-# #             obsTw = np.dot(sensorT, obsT)
-# #         else: 
-# #             obsTw = obsT
-
-# #         rpy = tf.euler_from_matrix(obsTw)
-
-# #         pose_list_msg.objs[j].id = j
-
-# #         pose_list_msg.objs[j].x = obsTw[0,3]
-# #         pose_list_msg.objs[j].y = obsTw[1,3]
-# #         pose_list_msg.objs[j].z = obsTw[2,3]
-
-# #         pose_list_msg.objs[j].roll = rpy[0]
-# #         pose_list_msg.objs[j].pitch = rpy[1]
-# #         pose_list_msg.objs[j].yaw = rpy[2]
-        
-# #     g_lc.publish("OBJ_COLLECTION", pose_list_msg.encode())
-# #     if verbose: print 'Published %i poses' % (nposes)
-
-# #     carr = plt.cm.Spectral(np.arange(len(arr)))
-# #     publish_point_cloud(pub_channel+'_POINTS', arr[:,:3], c=carr[:,:3])
-        
-
-# # ===== Pose drawing (viz) ====
-# def publish_text_list(pub_channel, ref_channel, texts, 
-#                       sensor_tf='camera'):
-#     """
-#     Publish Text List on:
-#     pub_channel: Channel on which the cloud will be published
-#     """
-#     frame_id = publish_sensor_frame(sensor_tf)
-#     frame_pose = get_frame(sensor_tf)
-#     assert(frame_id is not None)
-
-#     # # pose list collection msg
-#     # pose_list_msg = vs.obj_collection_t()
-#     # pose_list_msg.id = get_frame_id(pub_channel+'-text')
-#     # pose_list_msg.name = pub_channel+'-text'
-#     # pose_list_msg.type = vs.obj_collection_t.AXIS3D
-#     # pose_list_msg.reset = True
-
-#     text_list_msg = vs.text_collection_t()
-#     text_list_msg.name = pub_channel
-#     text_list_msg.id = frame_id
-#     text_list_msg.type = 1 # doesn't matter
-#     text_list_msg.reset = True
-    
-#     # nposes = len(arr) 
-
-#     # # fill out points
-#     # pose_list_msg.objs = [vs.obj_t() for j in range(0,nposes)]
-#     # pose_list_msg.nobjs = nposes             
-    
-#     nposes = len(texts)
-#     text_list_msg.texts = [vs.text_t() for j in range(0,nposes)]
-#     text_list_msg.n = nposes
-
-#     # sensorT = frame_pose.matrix
-#     # print sensorT
-
-#     for j in range(nposes):
-#         # obsT = tf.quaternion_matrix(np.array([1,0,0,0]))
-#         # obsT[:3,3] = arr[j,:3]
-#         # obsTw = np.dot(sensorT, obsT)
-
-#         # rpy = tf.euler_from_matrix(obsTw)
-
-#         # tf_arr = frame_pose * arr[j,:3]
-
-#         # pose_list_msg.objs[j].id = j
-
-#         # pose_list_msg.objs[j].x = obsTw[0,3]
-#         # pose_list_msg.objs[j].y = obsTw[1,3]
-#         # pose_list_msg.objs[j].z = obsTw[2,3]
-
-#         # pose_list_msg.objs[j].roll = 0 # rpy[0]
-#         # pose_list_msg.objs[j].pitch = 0 # rpy[1]
-#         # pose_list_msg.objs[j].yaw = 0 # rpy[2]
-
-#         text_list_msg.texts[j].id = j
-#         text_list_msg.texts[j].collection_id = get_frame_id(ref_channel)
-#         text_list_msg.texts[j].object_id = j
-#         text_list_msg.texts[j].text = texts[j]
-        
-#     # g_lc.publish("OBJ_COLLECTION", pose_list_msg.encode())
-#     g_lc.publish("TEXT_COLLECTION", text_list_msg.encode())
-#     # g_log.debug('Published %i poses' % (nposes))
-
-
-
-#     # carr = plt.cm.Spectral(np.arange(len(arr)))
-#     # publish_point_cloud(pub_channel+'_POINTS', arr[:,:3], c=carr[:,:3])
-
-
-# # LCMGL Calls ==============================================================
-# def publish_point_cloud_lcmgl(pub_channel, arr, c='r', size=1, alpha=1):
-#     """ 
-#     Publish point cloud array in lcmgl
-#     """
-#     arr = reshape_arr(arr)    
-#     g = lcmgl.lcmgl('posepairs', lc)
-#     g.glPointSize(size)
-#     g.glColor4f(c[0],c[1],c[2],alpha)
-#     g.glBegin(lcmgl.GL_POINTS)
-#     for j in range(0,len(arr)):
-#         g.glVertex3d(arr[j,0], arr[j,1], arr[j,2])
-#     g.glEnd()
-#     g.switch_buffer()
-
-# def publish_text_lcmgl(pub_channel, arr, texts, alpha=0.9, sensor_tf='camera'): 
-#     """ 
-#     Publish text with point cloud array in lcmgl
-#     """
-#     frame_id = publish_sensor_frame(sensor_tf)
-#     frame_pose = get_frame(sensor_tf)
-#     assert(frame_id is not None)
-
-#     # Setup lcmgl text
-#     g = lcmgl.lcmgl(pub_channel, lc)
-
-#     # w.r.t sensor frame
-#     arr = frame_pose * arr
-
-#     # Plot
-#     g.glColor4f(0.2,0.2,0.2,alpha)
-#     for idx, text in enumerate(texts):
-#         g.text(arr[idx,0],arr[idx,1],arr[idx,2], text)
-#     g.switch_buffer()
+def publish_quads(pub_channel, quads, frame_id='camera', reset=True):
+    publish_point_type(pub_channel, quads, point_type='QUADS', frame_id=frame_id, reset=reset)
 
 # Object Renderers ==============================================================
 def draw_camera(pose, zmin=0.0, zmax=0.1, fov=np.deg2rad(60)): 
@@ -778,10 +408,21 @@ def draw_laser_frustum(pose, zmin=0.0, zmax=10, fov=np.deg2rad(60)):
     edges = np.vstack(edges)
     return (faces, edges)
 
+def draw_tag(pose=None, size=0.1):
+    sz = size / 2.0
+    corners = np.float32([[sz, sz, 0], [-sz, sz, 0], 
+                          [-sz, -sz, 0], [sz, -sz, 0], 
+                          [sz, sz, 0], [-sz, -sz, 0], 
+                          [-sz, sz, 0], [sz, -sz, 0]])
+    return pose * corners if pose is not None else corners
 
-def publish_quads(pub_channel, quads, frame_id='camera', reset=True):
-    publish_point_type(pub_channel, quads, point_type='QUADS', frame_id=frame_id, reset=reset)
+def draw_tag_edges(pose=None, size=0.1):
+    return corners_to_edges(draw_tag(pose=pose, size=size))
     
+def draw_tags_edges(poses, size=0.1): 
+    return np.vstack([draw_tag_edges(p) for p in poses])
+
+# Publish Objects ===============================================================
 def publish_cameras(pub_channel, poses, c='y', texts=[], covars=[], frame_id='camera', 
                     draw_faces=False, draw_edges=True, draw_nodes=False, size=1, zmin=0.01, zmax=0.3, reset=True):
     cam_feats = [draw_camera(pose, zmin=zmin * size, zmax=zmax * size) for pose in poses]
@@ -827,5 +468,52 @@ def publish_laser_frustums(pub_channel, poses, c='y', texts=[], frame_id='camera
         carr = [c] * len(cam_faces)
         publish_point_type(pub_channel+'-faces', cam_faces, point_type='TRIANGLES', c=carr, frame_id=frame_id, reset=reset)
 
+# # @run_async
+# def publish_point_type(pub_channel, arr, c='r', point_type='POINT', 
+#                        flip_rb=False, frame_id='camera', element_id=0, reset=True):
+#     _publish_point_type(pub_channel, arr, c=c, point_type=point_type, 
+#                         flip_rb=flip_rb, frame_id=frame_id, element_id=element_id, reset=reset)
 
+# # @run_async
+# def publish_pose_list(pub_channel, poses, texts=[], covars=[], frame_id='camera', reset=True, object_type='AXIS3D'):
+#     _publish_pose_list(pub_channel, deepcopy(poses), texts=texts, covars=covars, 
+#                        frame_id=frame_id, reset=reset, object_type=object_type)
 
+# # @run_async
+# def publish_line_segments(pub_channel, arr1, arr2, c='r', flip_rb=False, frame_id='camera'):
+#     _publish_line_segments(pub_channel, deepcopy(arr1), deepcopy(arr2), c=deepcopy(c), flip_rb=flip_rb, frame_id=frame_id)
+
+# # LCMGL Calls ==============================================================
+# def publish_point_cloud_lcmgl(pub_channel, arr, c='r', size=1, alpha=1):
+#     """ 
+#     Publish point cloud array in lcmgl
+#     """
+#     arr = reshape_arr(arr)    
+#     g = lcmgl.lcmgl('posepairs', lc)
+#     g.glPointSize(size)
+#     g.glColor4f(c[0],c[1],c[2],alpha)
+#     g.glBegin(lcmgl.GL_POINTS)
+#     for j in range(0,len(arr)):
+#         g.glVertex3d(arr[j,0], arr[j,1], arr[j,2])
+#     g.glEnd()
+#     g.switch_buffer()
+
+# def publish_text_lcmgl(pub_channel, arr, texts, alpha=0.9, sensor_tf='camera'): 
+#     """ 
+#     Publish text with point cloud array in lcmgl
+#     """
+#     frame_id = publish_sensor_frame(sensor_tf)
+#     frame_pose = get_frame(sensor_tf)
+#     assert(frame_id is not None)
+
+#     # Setup lcmgl text
+#     g = lcmgl.lcmgl(pub_channel, lc)
+
+#     # w.r.t sensor frame
+#     arr = frame_pose * arr
+
+#     # Plot
+#     g.glColor4f(0.2,0.2,0.2,alpha)
+#     for idx, text in enumerate(texts):
+#         g.text(arr[idx,0],arr[idx,1],arr[idx,2], text)
+#     g.switch_buffer()
