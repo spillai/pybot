35d0aebb2e43fe5442a4bf8e34cf3c6c24f3ac80
Sudeep Pillai <spillai@csail.mit.edu>
2016-08-06 19:01:00 -0400

    added prepare segnet training data

--- nnet/segnet.py
+++ nnet/segnet.py
@@ -59,6 +59,32 @@ class UnPooling2D(Layer):
     def get_config(self):
         return {"name":self.__class__.__name__,
             "poolsize":self.poolsize}
+
+
+class UnPooling2D(Layer):
+    """A 2D Repeat layer"""
+    def __init__(self, poolsize=(2, 2)):
+        super(UnPooling2D, self).__init__()
+        self.poolsize = poolsize
+
+    @property
+    def output_shape(self):
+        input_shape = self.input_shape
+        return (input_shape[0], input_shape[1],
+                self.poolsize[0] * input_shape[2],
+                self.poolsize[1] * input_shape[3])
+
+    def get_output(self, train):
+        X = self.get_input(train)
+        s1 = self.poolsize[0]
+        s2 = self.poolsize[1]
+        output = X.repeat(s1, axis=2).repeat(s2, axis=3)
+        return output
+
+    def get_config(self):
+        return {"name":self.__class__.__name__,
+            "poolsize":self.poolsize}
+
     
 def segnet(input_shape, num_classes): 
     """
