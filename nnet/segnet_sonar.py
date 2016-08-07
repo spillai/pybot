a20e6179ccb25467e671f43778be89ec13e3a6c1
Sudeep Pillai <spillai@csail.mit.edu>
2016-08-07 09:44:04 -0400

    added sample weight for dataset generator

--- nnet/segnet_sonar.py
+++ nnet/segnet_sonar.py
@@ -15,7 +15,6 @@ os.environ['KERAS_BACKEND'] = 'tensorflow'
 
 import tensorflow as tf
 from keras import backend as K
-
 import keras.models as models
 from keras.layers.noise import GaussianNoise
 from keras.layers.core import Layer, Dense, Dropout, Activation, Flatten, Reshape, Merge, Permute
@@ -104,8 +103,8 @@ def binarylab(labels, num_classes):
         x[:,:,j][valid] = 1
     return x.reshape(H*W,num_classes)
 
-def data_generator(path, num_classes):
-    txt = np.loadtxt(os.path.join(path, 'train.txt'), dtype=np.str)
+def data_generator(path, num_classes, name='train.txt'):
+    txt = np.loadtxt(os.path.join(path, name), dtype=np.str)
     while True: 
         for (x,y) in txt:
             try: 
@@ -118,9 +117,12 @@ def data_generator(path, num_classes):
                 
             X_ = normalized(full_train_img)[:,:,np.newaxis]
             X = np.rollaxis(X_, 2)
+
             Y = binarylab(full_label_img, num_classes)
-            # print X.shape, Y.shape
-            yield np.asarray([X]), np.asarray([Y])
+            W = full_label_img.ravel() * (512 * 96.0 - 1) + 1.0
+            # print X.shape, Y.shape, W.shape
+
+            yield np.asarray([X]), np.asarray([Y]) # , np.asarray([W])
         
 def chunked_data(iterable, batch_size=10):
     for batch in chunks(iterable, batch_size): 
@@ -153,12 +155,14 @@ if __name__ == "__main__":
     # Setup Segnet training
     C, H, W = (nchannels, 512, 96)
     num_classes = 2
-    nb_epoch = 3000
-    samples_per_epoch = 30
+    nb_epoch = 1000
+    samples_per_epoch = 10
+    validation_samples_per_epoch = 5
     batch_size = 1
 
     # Data Generator
-    datagen = data_generator(os.path.expanduser(args.directory), num_classes)
+    datagen = data_generator(os.path.expanduser(args.directory), num_classes, name='train.txt')
+    validation_datagen = data_generator(os.path.expanduser(args.directory), num_classes, name='test.txt')
     chunked_datagen = chunked_data(datagen, batch_size=batch_size)
 
     try: 
@@ -169,17 +173,19 @@ if __name__ == "__main__":
     
     # chunked_X = imap(lambda item: item[0], chunked_datagen)
     # chunked_Y = imap(lambda item: item[1], chunked_datagen)
-    # for idx, (x,y) in enumerate(datagen):
-    #     print x.shape, y.shape
+    # for idx, (x,y,s) in enumerate(datagen):
+    #     print x.shape, y.shape, s.shape
     #     if idx > 100: 
     #         break
+
     
+    dev_str = '/gpu:' + args.gpu if int(args.gpu) >= 0 else '/cpu:0' 
     if len(args.model): 
         model = models.load_model(os.path.expanduser(args.model))
     else: 
-        with tf.device('/gpu:' + args.gpu):
+        with tf.device(dev_str):
             model = segnet(input_shape=(C, H, W), num_classes=num_classes)
-            model.compile(loss="categorical_crossentropy", metrics=['accuracy'], optimizer='adam')
+            model.compile(loss="categorical_crossentropy", metrics=['accuracy'], optimizer='adam', sample_weight_mode=None)
 
             # current_dir = os.path.dirname(os.path.realpath(__file__))
             # model_path = os.path.join(current_dir, "model.png")
@@ -187,7 +193,7 @@ if __name__ == "__main__":
 
             # SVG(vutil.to_graph(model, recursive=True, show_shape=True).create(prog='dot', format="svg"))
 
-            class_weight = [1.0, 512.0 * 96]
+            class_weight = [1.0 / (512 * 96), 1.0]
             callbacks = [
                 ProgbarLogger(),
                 TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True), 
@@ -196,7 +202,8 @@ if __name__ == "__main__":
             ]
 
             history = model.fit_generator(datagen, samples_per_epoch=samples_per_epoch, nb_epoch=nb_epoch,
-                                          show_accuracy=True, verbose=2, callbacks=callbacks, class_weight=class_weight) 
+                                          show_accuracy=True, verbose=2, callbacks=callbacks, class_weight=class_weight, 
+                                          validation_data=validation_datagen, nb_val_samples=validation_samples_per_epoch)
 
             model.save('models/model.hdf5')
 
@@ -205,7 +212,7 @@ if __name__ == "__main__":
     # colors = cv2.imread(args.colors).astype(np.uint8)
     
     # print('Predict')
-    # with tf.device('/gpu:' + args.gpu):
+    # with tf.device(dev_str):
     #     for idx, (im,target) in enumerate(datagen):
     #         gt = np.squeeze(target).argmax(axis=1).reshape(H,W).astype(np.uint8)
             
