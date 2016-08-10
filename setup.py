371726ff9af5798f025722b90ce032396554e150
Sudeep Pillai <spillai@csail.mit.edu>
2016-08-10 17:31:55 -0400

    added setup.py and modified license

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='pybot',
+      version='0.0.1',
+      description='Research tools for autonomous systems using Python',
+      author='Sudeep Pillai',
+      author_email='spillai@csail.mit.com',
+      url='https://github.com/spillai/pybot',
+      download_url='https://github.com/pybot/tarball/0.0.1',
+      license='MIT',
+      # install_requires=['theano', 'pyyaml', 'six'],
+      # extras_require={
+      #               'h5py': ['h5py'],
+      # },
+      packages=find_packages())
+      })
