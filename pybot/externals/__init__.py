dac9bb94be330b10191177183be2b5d12dbc2f05
Sudeep Pillai <spillai@csail.mit.edu>
2016-08-29 16:51:38 -0400

    nop support for draw_utils

--- pybot/externals/__init__.py
+++ pybot/externals/__init__.py
@@ -1,1 +1,26 @@
-# import pybot_externals as wrappers
+from collections import defaultdict
+
+class MayBeCalled(object):
+    def __call__(self, *args, **kwargs):
+        return None
+
+class nop(object):
+    """
+    Nop class to handle misc optional imports
+    Shamelessly ripped off 
+    from http://stackoverflow.com/questions/24946321/how-do-i-write-a-no-op-or-dummy-class-in-python
+    """
+    def __init__(self, name=''): 
+        self.name_ = name
+
+    def __get__(self, *args):
+        return MayBeCalled()
+
+    def __hasattr__(self, attr):
+        if len(self.name_): print('{}::{}'.format(self.name_, attr))
+        return True
+
+    def __getattr__(self, attr):
+        if len(self.name_): print('{}::{}'.format(self.name_, attr))
+        return MayBeCalled()
+
--- pybot/externals/draw_utils.py
+++ pybot/externals/draw_utils.py
@@ -1,5 +1,5 @@
 # Author: Sudeep Pillai <spillai@csail.mit.edu>
 # License: MIT
 
-# from pybot.externals.ros.draw_utils import *
 from pybot.externals.lcm.draw_utils import *
+    
