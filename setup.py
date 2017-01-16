aa65ef2a21703c087013374298a99d6b136b7350
Sudeep Pillai <spillai@csail.mit.edu>
2017-01-16 16:28:25 -0500

    minor url fix in setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import find_packages
 
 setup(name='pybot',
       version='0.1',
-      description='Research tools for autonomous systems using Python',
+      description='Research tools for autonomous systems in Python',
       author='Sudeep Pillai',
       author_email='spillai@csail.mit.com',
       url='https://github.com/spillai/pybot',
-      download_url='https://github.com/pybot/tarball/0.0.1',
+      download_url='https://github.com/spillai/pybot/archive/pybot-v0.1.tar.gz',
       license='MIT',
       # install_requires=['tables', 'six'],
       # extras_require={
