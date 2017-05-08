6afa2e88882f3d7acffaaec4186c5182ae1e2077
Sudeep Pillai <spillai@csail.mit.edu>
2017-05-08 17:08:36 -0400

    better environment support for pybot

--- pybot/__init__.py
+++ pybot/__init__.py
@@ -2,8 +2,13 @@ import os
 
 def get_environment(env_str, default, choices=None):
     var = os.getenv(env_str, default)
+    if var is None: 
+        raise RuntimeError('{} not in environment'.format(env_str))
+
     if choices is not None:
-        assert var in set(choices)
+        if var not in set(choices):
+            raise RuntimeError('{}: {} not in choices {}'.format(env_str, var, choices))
+        
     return var
 
 _PYBOT_USER_DATA = get_environment('PYBOT_USER_DATA',
