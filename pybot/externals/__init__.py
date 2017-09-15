1f2c95b208cd306498d52a4312eb9006e7a26c5c
Sudeep Pillai <spillai@csail.mit.edu>
2017-09-14 23:00:51 -0400

    hotswap marshalling and vis-serialization backends

--- pybot/externals/__init__.py
+++ pybot/externals/__init__.py
@@ -1,30 +1,59 @@
 import sys
 from collections import defaultdict
 from pybot import get_environment
+from pybot.utils.misc import color_green
 
+_PYBOT_VIS_CHOICES = ['lcm', 'protobuf']
 _PYBOT_VIS_BACKEND = get_environment('PYBOT_VIS_BACKEND',
                                       default='lcm',
-                                      choices=['lcm', 'protobuf'])
+                                      choices=_PYBOT_VIS_CHOICES)
+
+_PYBOT_MARSHALLING_CHOICES = ['lcm', 'zmq']
+_PYBOT_MARSHALLING_BACKEND = get_environment('PYBOT_MARSHALLING_BACKEND',
+                                      default='lcm',
+                                      choices=_PYBOT_MARSHALLING_CHOICES)
 
 def vis_backend():
     return _PYBOT_VIS_BACKEND
 
 def set_vis_backend(value):
+    assert(value in set(_PYBOT_VIS_CHOICES))
     _PYBOT_VIS_BACKEND = value
+
+def marshalling_backend():
+    return _PYBOT_MARSHALLING_BACKEND
+
+def set_marshalling_backend(value):
+    assert(value in set(_PYBOT_MARSHALLING_CHOICES))
+    _PYBOT_MARSHALLING_BACKEND = value
 
 # ===============================================================================
 
 if _PYBOT_VIS_BACKEND == 'lcm':
-    sys.stderr.write('Using lcm-vis backend.\n')
     from pybot.externals.lcm import vs, serialize
     
 elif _PYBOT_VIS_BACKEND == 'protobuf':
-    sys.stderr.write('Using protobuf-vis backend.\n')
     from pybot.externals.pb import vs, serialize
 
 else:
-    raise Exception('Unknown backend: {}'.format(cfg.SLAM_BACKEND))
+    raise Exception('''Unknown backend: {}, '''
+                    '''choices are {}'''.format(_PYBOT_VIS_BACKEND,
+                                                _PYBOT_VIS_CHOICES))
+print('Using {} vis backend.'.format(color_green(_PYBOT_VIS_BACKEND)))
+
+# ===============================================================================
+
+if _PYBOT_MARSHALLING_BACKEND == 'lcm':
+    from pybot.externals.lcm import publish
     
+elif _PYBOT_MARSHALLING_BACKEND == 'zmq':
+    from pybot.externals.zeromq import publish
+
+else:
+    raise Exception('''Unknown backend: {}, '''
+                    '''choices are {}'''.format(_PYBOT_MARSHALLING_BACKEND,
+                                                _PYBOT_MARSHALLING_CHOICES))
+print('Using {} marshalling backend.'.format(color_green(_PYBOT_MARSHALLING_BACKEND)))
 
 # ===============================================================================
 
