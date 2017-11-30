8ad73e489e613d868f83f4ce68f979b401b89161
Sudeep Pillai <spillai@csail.mit.edu>
2017-11-30 12:11:44 -0500

    cleaned up viewer scripts

--- viewer/run.sh
+++ viewer/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# A single script that spawns a simple http.server, and
+# a zeromq-based server that relays data to a websocket
+# that the client browser can connect to.
+
 echo "Go to localhost:8000/viewer.html";
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
